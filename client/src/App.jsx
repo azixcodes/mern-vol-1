9c7f46694f3666e65cc9c3626fc348d23c986ddc
azezcodes <aziz@swatitech.com>
2023-11-22 17:56:34 +0500

    new design

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ const App = () => {
   const [data, setData] = useState([]);
   const [input, setInput] = useState("");
   const [filteredData, setFilteredData] = useState([]);
+  const [selectedUser, setSelectedUser] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -26,7 +27,10 @@ const App = () => {
     );
     setFilteredData(filtered);
   };
-  console.log(data);
+  const handleItemClick = (item) => {
+    setSelectedUser(item);
+  };
+  console.log(selectedUser);
   return (
     <main className="flex w-full h-screen justify-center px-2 py-4 bg-white dark:bg-slate-900">
       <div className="w-full max-w-sm rounded-md border border-gray-300 dark:border-gray-500 shadow-md px-2 py-2 flex flex-col gap-4 h-fit bg-white text-black dark:bg-slate-900 dark:text-white">
@@ -36,7 +40,7 @@ const App = () => {
         <div className="flex border relative h-8 rounded-md items-center">
           <MagnifyingGlassIcon className="h-6 w-6 text-gray-400" />
           <input
-            className="h-full w-full rounded-md outline-none focus:outline-none px-1"
+            className="h-full w-full rounded-md outline-none focus:outline-none px-1 dark:bg-transparent"
             placeholder="Search accounts"
             onChange={(e) => setInput(e.target.value)}
             onKeyUp={handleSearch}
@@ -44,8 +48,10 @@ const App = () => {
         </div>
         {filteredData.map((user, index) => (
           <div
-            className="flex gap-2 items-center w-full flex-row max-wxs shadow-lg border dark:border-gray-500 border-gray-300 rounded-md p-2 "
+            className="flex gap-2 items-center w-full flex-row max-wxs shadow-lg border dark:border-gray-500
+             border-gray-300 rounded-md p-2 hover:ring-1 hover:cursor-pointer"
             key={index}
+            onClick={() => handleItemClick(user)}
           >
             <img
               src={user.avatar}
@@ -53,7 +59,9 @@ const App = () => {
               alt={user.username}
             />
             <div className="flex flex-col">
-              <h4 className=" font-semibold text-sm">{user.username}</h4>
+              <h4 className=" font-semibold text-sm">
+                {user.username.slice(1)}
+              </h4>
               <a
                 href={`mailto:${user.email}`}
                 className="text-gray-400 dark:text-gray-300 text-xs dark:hover:text-sky-500 hover:text-sky-500 transition-colors duration-75 ease-out"
@@ -64,6 +72,20 @@ const App = () => {
           </div>
         ))}
       </div>
+      {selectedUser && (
+        <div className="fixed right-20 h-48 w-48 p-2 flex flex-col border shadow-lg top-[50%] translate-y-[-50%]">
+          <img
+            src={selectedUser.avatar}
+            className="h-16 w-16 rounded-full p-0.5 border mx-auto object-cover"
+          />
+          <h4 className="text-center text-sm font-semibold">
+            {selectedUser.tagline}
+          </h4>
+          <div className="flex justify-center">
+            
+          </div>
+        </div>
+      )}
     </main>
   );
 };
